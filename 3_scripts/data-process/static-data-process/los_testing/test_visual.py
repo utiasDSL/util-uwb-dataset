0a44a20c6b64c7ead7f7aade89c465f4d4fa144e
wenda <wenda.zhao@robotics.utias.utoronto.ca>
2021-05-13 23:31:12 -0400

    update

--- 3_scripts/data-process/static-data-process/los_testing/test_visual.py
+++ 3_scripts/data-process/static-data-process/los_testing/test_visual.py
@@ -31,13 +31,28 @@ err12 = np.load(curr+'/circle_test1/0513_circle1_test12_err.npy')
 data = [err1, err2, err3, err4, err5, err6,
         err7, err8, err9, err10, err11, err12]
   
+
+circle2_err1 = np.load(curr+'/circle_test2/0513_circle2_test1_err.npy')
+circle2_err2 = np.load(curr+'/circle_test2/0513_circle2_test2_err.npy')
+
+circle2_err3 = np.load(curr+'/circle_test2/0513_circle2_test3_err.npy')
+circle2_err4 = np.load(curr+'/circle_test2/0513_circle2_test4_err.npy')
+
+circle2_err5 = np.load(curr+'/circle_test2/0513_circle2_test5_err.npy')
+circle2_err6 = np.load(curr+'/circle_test2/0513_circle2_test6_err.npy')
+
+data2 = [circle2_err1, circle2_err2, circle2_err3,
+         circle2_err4, circle2_err5, circle2_err6]
+
 fig = plt.figure()
-  
+fig1 = plt.figure()
 # Creating axes instance
 ax = fig.add_subplot(111)
 # Creating plot
-bp = ax.boxplot(data)
+ax.boxplot(data)
   
+bx = fig1.add_subplot(111)
+bx.boxplot(data2)
 # show plot
 plt.show()
 
