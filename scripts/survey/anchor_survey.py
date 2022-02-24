correct the survey quaternion (seq. of const4 in raw data)

scripts/survey/anchor_survey.py
@@ -194,9 +194,9 @@ if __name__ == "__main__":
     for i in range(An_num):
         # three markers in the inertial frame
         dest_points = Marker_pos[idx:idx+3,:]
-        # 3 markers in the Anchor frame. Order: [left, up, forward]
-        src_points = np.array([[0.0,   110.0,   0.0],
-                               [121.0,   0.0,   0.0],
+        # 3 markers in the Anchor frame. Order: [up, left, forward]
+        src_points = np.array([[110.0,   0.0,   0.0],
+                               [0.0,   121.0,   0.0],
                                [0.0,     0.0,   80.0]]) / 1000.0 
         # compute the rotation from body to inertial frame
         (C_survey, t_est) = pointcloud_alignment(src_points, dest_points)   